1050b088e0335b61b14f90524325804d355d3e29
26624 <2662401190@qq.com>
2020-06-21 23:33:12 +0800

    init

---
+++ src/main/java/com/example/weimao/WeimaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.weimao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeimaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeimaoApplication.class, args);
+    }
+
+}
